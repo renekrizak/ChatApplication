8ffe4240a0b75d917d535fc7ca9157b72f7b2031
René Križák <rene.krizak@gmail.com>
2022-11-01 12:10:22 +0100

    changes

--- ChatApplication/ViewModel/ClientViewModel.cs
+++ ChatApplication/ViewModel/ClientViewModel.cs
@@ -17,21 +17,27 @@ using ChatClient.Store;
 using ChatClient.View;
 using ChatClient.Net;
 using System.Net.Sockets;
+using ChatClient.Core;
+
 
 namespace ChatClient.ViewModel
 {
     public class ClientViewModel : ViewModelBase
     {
-
+        public RelayCommand SendMessage { get; set; }
         private string _username;
+        private string _password;
+        private string _email;
         private string _uid;
         private string _message;
+        private Server _server;
         
         public string Message
         {
             get { return _message; }
             set
             {
+                Debug.WriteLine(Message);
                 _message = value;
                 ObjPropertyChanged();
             }
@@ -39,10 +45,27 @@ namespace ChatClient.ViewModel
         
         public ICommand SendMessageCommand { get; }
 
-        public ClientViewModel()
+        public ClientViewModel(string username, string password)
         {
-
+            _username = username;
+            _password = password;
+            Debug.WriteLine($"{_username}|{_password}");
+            string logInfo = $"{_username}|{_password}";
+            _server = new Server();
+            _server.LoginConnectToServer(logInfo);
+            SendMessage = new RelayCommand(o => _server.SendMessageToServer(Message), o => !string.IsNullOrEmpty(Message));
         }
-       
+
+        public ClientViewModel(string username, string email, string password)
+        {
+            _username = username;
+            _email = email;
+            _password = password;
+            Debug.WriteLine($"{_username}|{_email}|{_password}");
+            string regInfo = $"{_username}|{_email}|{_password}";
+            _server = new Server();
+            _server.RegisterConnectToServer(regInfo);
+        }
+
     }
 }
