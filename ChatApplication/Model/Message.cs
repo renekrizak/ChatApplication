7e35381ed9d8b4534ff2b6689045efb98088eb4c
renekrizak <rene.krizak@gmail.com>
2022-11-14 15:32:02 +0100

    need to fix text inside message bubble

--- ChatApplication/Model/Message.cs
+++ ChatApplication/Model/Message.cs
@@ -6,5 +6,10 @@
         public string messageUsername { get; set; }
         public string messageContent { get; set; }
 
+        public double messageWidth { get; set; }
+        
+
+        public double messageHeight { get; set; }
+
     }
 }
